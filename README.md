881be535d7ce5eac6d716ceaab3ab89eb2331f6e
gohil.varun@iitgn.ac.in <gohil.varun@iitgn.ac.in>
2018-11-07 21:43:26 +0530

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Face-Recognition-System-using-Eignen-Values
+
+
+
+### Dependencies
+
+- Python 3.4
+- OpenCV
+- Numpy
+
+### How to Run?
+
+There are two ways to run the program:
+
+- Recognize faces for all test images
+
+```shell
+python main.py
+```
+
+- Recognizes face for a specified image
+
+```
+python main.py <path_to_image>
+```
+
+
+
+### Output Format
+
+- When we recognize faces for all test images, the output is a table in which the first column specifies the expected output, the second column specifies the actual output and the third column wheter the recognition was correct or not. 
+
+- When we recognize face in a specified image . The output ranges from s1 till s40.
+
+  For example, If the output is s4 then that means that the test image is recognized to be the person whose images are in folder s4.
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import sys
 import os
 
 def dataFetcher():
@@ -10,17 +11,12 @@ def dataFetcher():
 	Returns a list of 1D matrices of all the training images. 
 	"""
 	imgVectorList = []
-	dataDir = "orl_faces"
-	fileList = os.listdir(dataDir)
-	for item in fileList:
-		itemPath = os.path.join(dataDir, item)
-		if os.path.isdir(itemPath):
-			subFileList = os.listdir(itemPath)
-			for img in subFileList:
-				imgPath = os.path.join(dataDir,item, img)
-				imgMatrix = cv.imread(imgPath, 0)
-				imgVector = imgMatrix.flatten()
-				imgVectorList.append(imgVector)
+	for i in range(1,41):
+		for j in range(1,10):
+			imgPath = os.path.join("orl_faces", "s" + str(i), str(j) + ".pgm")
+			imgMatrix = cv.imread(imgPath, 0)
+			imgVector = imgMatrix.flatten()
+			imgVectorList.append(imgVector)
 	return imgVectorList
 
 
@@ -51,17 +47,21 @@ def makeCovarianceMatrix(OffsetVectorList):
 
 def computeEigenVectors(OffsetVectorList):
 	"""
-	Computes the eigrn values and eigen vectors of the covariance matrix
+	Computes the eigen values and eigen vectors of the covariance matrix
 	"""
 	altMatrix = np.dot(np.array(OffsetVectorList),np.array(OffsetVectorList).transpose())
 	eigenValueArray, eigenVectorMatrix = np.linalg.eig(altMatrix)
 	eigenVectorList = []
 	for i in range(len(eigenVectorMatrix)):
 		eigenVectorList.append(np.dot(np.array(OffsetVectorList).transpose(),eigenVectorMatrix[i]))
+	for eigenVector in eigenVectorList:
+		eigenVector = eigenVector/float(np.linalg.norm(eigenVector))
 	return eigenValueArray.tolist(), eigenVectorList
 
-def selectKeigenVectors(eigenValueList, eigenVectorList, K = 300):
-	"Selects K eigen vectors corresponding to maximum K eigen values"
+def selectKeigenVectors(eigenValueList, eigenVectorList, K = 20):
+	"""
+	Selects K eigen vectors corresponding to maximum K eigen values
+	"""
 	selectedEigenVectors = []
 	while len(selectedEigenVectors) < K:
 		maxEigenValue = max(eigenValueList)
@@ -71,15 +71,83 @@ def selectKeigenVectors(eigenValueList, eigenVectorList, K = 300):
 		del eigenValueList[index]
 	return selectedEigenVectors
 
-if __name__ == "__main__":
+def makeWeightVectors(selectedEigenVectors, OffsetVectorList):
+	"""
+	Projects all the image vectors in space of the selected eigen vectors and returns the list of weight vectors.
+	"""
+	weightVectorList = []
+	selectedEigenMatrix = np.array(selectedEigenVectors)
+	for offsetVector in OffsetVectorList:
+		weightVectorList.append(np.dot(selectedEigenMatrix,offsetVector))
+	return weightVectorList
+
+
+def train():
+	print("Training...")
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*0, 0))
 	imgVectorList = dataFetcher()
-	print len(imgVectorList)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*10, 1*(100/7)))
 	avgImgVector = findAvgImgVector(imgVectorList)
-	print avgImgVector.shape
-	OffsetVectorList = makeOffsetVectorList(imgVectorList, avgImgVector)
-	print len(OffsetVectorList), OffsetVectorList[0].shape
-	covarianceMatrix = makeCovarianceMatrix(OffsetVectorList)
-	print covarianceMatrix.shape
-	eigenValueList, eigenVectorList = computeEigenVectors(OffsetVectorList)
-	selectedEigenVectors = selectKeigenVectors(eigenValueList, eigenVectorList)
-	print len(selectedEigenVectors)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*20, 2*(100/7)))
+	offsetVectorList = makeOffsetVectorList(imgVectorList, avgImgVector)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*30, 3*(100/7)))
+	covarianceMatrix = makeCovarianceMatrix(offsetVectorList)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*40, 4*(100/7)))
+	eigenValueList, eigenVectorList = computeEigenVectors(offsetVectorList)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*50, 5*(100/7)))
+	try:
+		selectedEigenVectors = selectKeigenVectors(eigenValueList, eigenVectorList)
+	except:
+		selectedEigenVectors = selectKeigenVectors(eigenValueList, eigenVectorList)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*60, 6*(100/7)))
+	weightVectorList = makeWeightVectors(selectedEigenVectors, offsetVectorList)
+	sys.stdout.flush()
+	sys.stdout.write('\r')
+	sys.stdout.write("[%-70s] %d%%" % ('='*70, 100))
+	sys.stdout.write("\n")
+	return avgImgVector, np.array(selectedEigenVectors), weightVectorList
+
+
+def recognizeFace(avgImgVector, selectedEigenMatrix, weightVectorList, testImgPath):
+	"""
+	Computes disatance of test image with each image in dataset and accordingly outputs the predicted face.
+	"""
+	thresholdValue = 25000000
+	testImg = cv.imread(testImgPath,0).flatten()
+	normalizedTestImg = testImg - avgImgVector
+	weightedTestImg = np.dot(selectedEigenMatrix,normalizedTestImg)
+	minDist = np.inf
+	minDistIndex = -1
+	for index in range(len(weightVectorList)):
+		if np.linalg.norm(weightedTestImg - weightVectorList[index]) < minDist:
+			if np.linalg.norm(weightedTestImg - weightVectorList[index]) < thresholdValue:
+				minDist = np.linalg.norm(weightedTestImg - weightVectorList[index])
+				minDistIndex = index
+	if minDist > thresholdValue:
+		print("Could not recognize!")
+	return "s" + str(int(minDistIndex/9) + 1)	
+
+if __name__ == "__main__":
+	avgImgVector, selectedEigenMatrix,  weightVectorList = train()
+	try:
+		testImgPath = sys.argv[1]
+		print("Recognition started...")
+		print( recognizeFace(avgImgVector, selectedEigenMatrix, weightVectorList, testImgPath))
+	except:
+		for i in range(1,41):
+			testImgPath = os.path.join("orl_faces", "s" + str(i), "10.pgm")
+			predictedFace = recognizeFace(avgImgVector, selectedEigenMatrix, weightVectorList, testImgPath)
+			print("s" + str(i), predictedFace, "s" + str(i) == predictedFace)
